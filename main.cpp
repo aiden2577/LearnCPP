6819ab4dd6e56d7d96eb3301c2529c9fbda7ab3e
Aiden <3258000573@qq.com>
2025-10-12 23:16:15 +0800

    创建main.cpp文件，写了初始代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{
+	
+}
+
+int main()
+{
+	int arr[]={12,4,5,23,63,23};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+}
