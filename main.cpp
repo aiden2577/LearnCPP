434c8ec3782634c36e642dcdd9be9808d3298774
Aiden <3258000573@qq.com>
2025-10-15 00:36:36 +0800

    优化冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void sort(int arr[],int size)
 {
 	int tmp =0;
+	bool flag = false;
 	for (int i = 0; i < size-1; i++)
 	{
 		/* code */
@@ -17,9 +18,13 @@ void sort(int arr[],int size)
 				tmp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = tmp;
+				flag=true;
 			}
 		}
+	if(!flag)
+		break;
 	}
+	
 	
 	
 }
