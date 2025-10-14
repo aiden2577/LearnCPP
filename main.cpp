d79e2ee120d79472f88fa3be52657ad96ccd40b4
aiden2577 <3258000573@qq.com>
2025-10-15 00:30:23 +0800

    WanMing modify sort algorithm,Sort form largest to smallest

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ void sort(int arr[],int size)
 	int tmp =0;
 	for (int i = 0; i < size-1; i++)
 	{
-		/* code */
-		for (int j = 0; j < size-i-1; j++)
+		//wanMing modify sort algorithm,Sort from largest to smallest
+		for (int j = 0; j < size-i-1; ++j)
 		{
-			/* code */
-			if(arr[j]>arr[j+1])
+			if(arr[j]<arr[j+1])
 			{
 				tmp = arr[j];
 				arr[j] = arr[j+1];
